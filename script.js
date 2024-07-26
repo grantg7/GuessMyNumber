acc850345cbe5627fcbbbad5994807bc362ef782
grant <grant.enderkids@gmail.com>
2024-07-26 11:52:58 -0600

    i added dom changes to the .message, .score, and .number

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 //DOM maniplulation
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number😁';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
