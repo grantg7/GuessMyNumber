689ce735263ffb0f166e7d23eb03beb7b344d6b3
grant <grant.enderkids@gmail.com>
2024-07-29 21:37:44 -0600

    added the highscore element to the game. Now when the you play the game if the current score is larger than the last score than the high score will reflect that

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ document.querySelector('.guess').value = 23;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -35,6 +36,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
